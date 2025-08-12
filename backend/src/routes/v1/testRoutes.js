ffcef5c69b40ee88915cef8ee06783e209c8c24e
Isam <nfs.issamhajji@gmail.com>
2025-08-12 13:54:31 +0200

    Update testRoutes.js

--- backend/src/routes/v1/testRoutes.js
+++ backend/src/routes/v1/testRoutes.js
@@ -1,11 +1,10 @@
 const express = require("express");
-const itemController = require("../../controllers/itemController");
+const userController = require("../../controllers/userController");
 const router = express.Router();
 
 router.use(express.json());
 router.use(express.urlencoded({extended: true}));
 
-// router.get("/", itemController.getAllItems);
 router.get("/", (req,res) => {
     /* 
      * #swagger.tags = ['Test']
@@ -17,7 +16,7 @@ router.get("/", (req,res) => {
      }
     */
     /*  #swagger.parameters['parameterName'] = {in: 'query',description: 'Some description...', type: 'number'} */
-    itemController.getAllItems(req, res);
+    userController.getAllUsers(req, res);
 });
 
 
